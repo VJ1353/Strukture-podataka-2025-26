1099a75e03fbd6a28c2d68ed7ad5a5004fafd3a4
VJ1353 <viktoriaj1353@gmail.com>
2025-10-20 21:03:56 +0200

    Zadatak2

--- Vjezba2/Zadatak 2/Zad2.c
+++ Vjezba2/Zadatak 2/Zad2.c
@@ -1,16 +1,43 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #define _CRT_SECURE_NO_WARNINGS
 
-typedef struct osoba {
-	char ime;
-	char prezime;
-	int god_rodj = 0;
+//Struct declaration:
+typedef struct person* position
+struct person {
+	char name[];
+	char surname[];
+	int birthyear = 0;
+	position next;
 };
+
+//List of functions:
+void addToStart(person **head, char *name, char *surname, int year);
+void printlist(person *head);
+void addToEnd(person** head, char* name, char* surname, int year);
+person* findSurname(person* head, char* surname);
+void deletePerson(person** head, char* surname);
+
+
 
 int main() {
 
+	person* head = NULL;
 
+	//B) Print the list:
+    printf("Printed list:\");
+    printlist(head);
 
 
 	return 0;
+}
+
+
+void printlist(person* head) {
+    person* temp = head;
+    while (temp != NULL) {
+        printf("%s %s (%d)\n", temp->name, temp->surname, temp->birthyear);
+        temp = temp->next;
+    }
 }
