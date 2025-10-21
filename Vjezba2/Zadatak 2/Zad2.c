459b35f6b68f43b051e1e348c4cbcdf67e75db15
VJ1353 <viktoriaj1353@gmail.com>
2025-10-21 13:02:55 +0200

    Zadatak2

--- Vjezba2/Zadatak 2/Zad2.c
+++ Vjezba2/Zadatak 2/Zad2.c
@@ -15,16 +15,24 @@ struct person {
 //List of functions:
 void printlist(person *head);
 person* searchSurname(person* head, char* surname);
-person* 
+person* newPerson(char* ime, char* prezime, int godina);
+void addToStart(person** head, char* name, char* surname, int year);
+void addToEnd(person** head, char* name, char* surname, int year);
+
 
 
 int main() {
 
 	person* head = NULL;
+    //A) Add to the start of the list:
+    addToStart(&head, "Viktoria", "Jurisic", 2005);
 
 	//B) Print the list:
     printf("Printed list:\");
     printlist(head);
+
+    //C)Add to the end of the list:
+    addToEnd(&head, "Ana", "Filipovic", 2005);
 
     //D) Search by surname:
 	printf("Search by surname 'Jurisic':\n");
@@ -35,12 +43,15 @@ int main() {
     else
         printf("Surname not found.\n");
 
-
+    //E) Delete element from the list:
+    printf("\nDelete person: 'Jurisic'\n");
+    deletePerson(&head, "Jurisic");
+    printlist(head);
 
 	return 0;
 }
 
-
+//Print the list function:
 void printlist(person* head) {
     person* temp = head;
     while (temp != NULL) {
@@ -49,6 +60,7 @@ void printlist(person* head) {
     }
 }
 
+//Search by surname function:
 person* searchSurname(person* head, char* surname) {
 person* temp = head;
 while (temp != NULL) {
@@ -57,4 +69,66 @@ while (temp != NULL) {
     temp = temp->next;
 }
 return NULL;
+}
+
+//Create a new element:
+person* newPerson(char* ime, char* prezime, int godina) {
+    person* newPer = (person*)malloc(sizeof(person));
+    if (!newPer) {
+        printf("Error: Memory allocation failed!\n");
+        return NULL;
+    }
+    strcpy(newPer->name, name);
+    strcpy(newPer->surname, surname);
+    newPer->birthyear = year;
+    newPer->next = NULL;
+    return newPer;
+}
+
+//Add new element at the start:
+void addToStart(person** head, char* name, char* surname, int year) {
+    person* newPer = newPerson(name, surname, year);
+    if (!newPer) return;
+
+    newPer->next = *head;
+    *head = newPer;
+}
+
+//Add new element at the end:
+void addToEnd(person** head, char* name, char* surname, int year) {
+    person* newPer = newPerson(name, surname, year);
+    if (!newPer) return;
+
+    if (*head == NULL) {
+        *head = newPer;
+        return;
+    }
+
+    person* temp = *head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newPer;
+}
+
+//Delete element function:
+void deletePerson(person** head, char* surname) {
+    person* temp = *head, * prev = NULL;
+
+    while (temp != NULL && strcmp(temp->surname, surname) != 0) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        printf("Person not found.\n", prezime);
+        return;
+    }
+
+    if (prev == NULL) 
+        *head = temp->next;
+    else
+        prev->next = temp->next;
+
+    free(temp);
 }
